32290b5b8b448cf09d3dae06047dd95edf9e9320
iryan72 <iryan0702@gmail.com>
2019-11-15 12:43:55 -0800

    Rework card Expenses to scale less but proportional to gold spent

--- src/main/java/mari_mod/cards/Mari_Expenses.java
+++ src/main/java/mari_mod/cards/Mari_Expenses.java
@@ -20,12 +20,10 @@ public class Mari_Expenses extends AbstractMariCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 2;
-    private static final int DAMAGE = 12;
-    private static final int SCALING = 4;
-    private static final int UPGRADE_SCALING = 2;
+    private static final int DAMAGE = 14;
+    private static final int SCALING = 1;
+    private static final int UPGRADE_SCALING = 1;
     //private static final int GOLD_PER_SCALE = 10;
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
@@ -34,13 +32,10 @@ public class Mari_Expenses extends AbstractMariCard {
     public Mari_Expenses(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
 
-        this.baseBlock = SCALING;
-        this.block = this.baseBlock;
-
         this.baseDamage = DAMAGE;
         this.damage = this.baseDamage;
 
-        this.baseMagicNumber = DAMAGE;
+        this.baseMagicNumber = SCALING;
         this.magicNumber = this.baseMagicNumber;
     }
 
@@ -54,63 +49,25 @@ public class Mari_Expenses extends AbstractMariCard {
 
     @Override
     public void calculateCardDamage(AbstractMonster mo) {
-        int scaling = SCALING;
-        if(upgraded) scaling += UPGRADE_SCALING;
 
         int dam = DAMAGE;
 
-        this.baseDamage = dam + scaling * (MariMod.timesMariSpentGoldThisCombat);
+        this.baseDamage = dam + this.magicNumber * (MariMod.goldSpentByMariThisCombat/5);
         this.damage =  this.baseDamage;
 
         super.calculateCardDamage(mo);
 
-        if(this.damage > 0) {
-            String desc;
-            if(upgraded){
-                desc = UPGRADE_DESCRIPTION;
-            }else{
-                desc = DESCRIPTION;
-            }
-            this.rawDescription = desc + EXTENDED_DESCRIPTION[0];
-            this.initializeDescription();
-        }
-
-        this.forgetBlock();
     }
 
     @Override
     public void applyPowers() {
 
-        int scaling = SCALING;
-        if(upgraded) scaling += UPGRADE_SCALING;
-
         int dam = DAMAGE;
 
-        this.baseDamage = dam + scaling * (MariMod.timesMariSpentGoldThisCombat);
+        this.baseDamage = dam + this.magicNumber * (MariMod.goldSpentByMariThisCombat/5);
         this.damage =  this.baseDamage;
 
         super.applyPowers();
-
-        if(this.damage > 0) {
-            String desc;
-            if(upgraded){
-                desc = UPGRADE_DESCRIPTION;
-            }else{
-                desc = DESCRIPTION;
-            }
-            this.rawDescription = desc + EXTENDED_DESCRIPTION[0];
-            this.initializeDescription();
-        }
-
-        this.forgetBlock();
-    }
-
-
-    public void forgetBlock(){
-        this.baseBlock = SCALING;
-        if(this.upgraded) this.baseBlock += UPGRADE_SCALING;
-        this.block = this.baseBlock;
-        this.isBlockModified = false;
     }
 
     @Override
@@ -122,8 +79,7 @@ public class Mari_Expenses extends AbstractMariCard {
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            this.upgradeBlock(UPGRADE_SCALING);
-            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.upgradeMagicNumber(UPGRADE_SCALING);
             this.initializeDescription();
         }
     }
