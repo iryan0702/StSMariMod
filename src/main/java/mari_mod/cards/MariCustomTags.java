449bb971e88e49d03e3059f7daf108758b784acf
iryan72 <iryan0702@gmail.com>
2019-11-14 23:51:22 -0800

    Add card tags for a class card system

--- src/main/java/mari_mod/cards/MariCustomTags.java
+++ src/main/java/mari_mod/cards/MariCustomTags.java
@@ -8,4 +8,9 @@ public class MariCustomTags {
     @SpireEnum public static AbstractCard.CardTags RADIANCE;
     @SpireEnum public static AbstractCard.CardTags QUOTATIONS;
     @SpireEnum public static AbstractCard.CardTags KINDLE;
+
+    @SpireEnum public static AbstractCard.CardTags DRAMA;
+    @SpireEnum public static AbstractCard.CardTags ENERGY;
+    @SpireEnum public static AbstractCard.CardTags CORRUPT;
+    @SpireEnum public static AbstractCard.CardTags DANCE;
 }
