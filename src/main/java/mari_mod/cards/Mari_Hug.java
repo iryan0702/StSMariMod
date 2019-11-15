d84b1551642a74adaa55be5fab87eba3bec06ee0
iryan72 <iryan0702@gmail.com>
2019-11-14 23:49:44 -0800

    Change card Hug to always apply 2 stacks of debuffs, costs 1(0) instead

--- src/main/java/mari_mod/cards/Mari_Hug.java
+++ src/main/java/mari_mod/cards/Mari_Hug.java
@@ -17,9 +17,9 @@ public class Mari_Hug extends AbstractMariCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 0;
-    private static final int CONVERSION = 1;
-    private static final int UPGRADE_CONVERSION = 1;
+    private static final int COST = 1;
+    private static final int CONVERSION = 2;
+    private static final int UPGRADE_COST = 0;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
@@ -44,7 +44,7 @@ public class Mari_Hug extends AbstractMariCard {
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_CONVERSION);
+            upgradeBaseCost(UPGRADE_COST);
             this.initializeDescription();
         }
     }
