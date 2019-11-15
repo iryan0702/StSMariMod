5ad3bc26574baaf00e51045b54e9eeb96b0b6b9b
iryan72 <iryan0702@gmail.com>
2019-11-15 12:45:15 -0800

    Rework card Let's Go! to change cost based on attacks/skills played

--- src/main/java/mari_mod/cards/Mari_Lets_Go.java
+++ src/main/java/mari_mod/cards/Mari_Lets_Go.java
@@ -12,6 +12,7 @@ import com.megacrit.cardcrawl.powers.GainStrengthPower;
 import com.megacrit.cardcrawl.powers.LoseStrengthPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import mari_mod.MariMod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,7 +22,7 @@ public class Mari_Lets_Go extends AbstractMariCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 0;
+    private static final int COST = 1;
     private static final int DRAW = 3;
     private static final int UPGRADE_DRAW = 1;
     private static final CardType TYPE = CardType.SKILL;
@@ -41,38 +42,35 @@ public class Mari_Lets_Go extends AbstractMariCard {
     }
 
     @Override
-    public void applyPowers() {
-        super.applyPowers();
-        int otherSkills = 0;
-        for (AbstractCard card : AbstractDungeon.player.hand.group) {
-            if (card.type == CardType.SKILL && card != this) otherSkills++;
-
-            if(card instanceof Mari_Stewshine){
-                Mari_Stewshine stewshine = (Mari_Stewshine) card;
-                if(stewshine.card1.uuid.equals(this.uuid)){
-                    otherSkills--;
-                }else if(stewshine.card2.uuid.equals(this.uuid)){
-                    otherSkills--;
-                }else if(stewshine.card3.uuid.equals(this.uuid)){
-                    otherSkills--;
-                }
-            }
-        }
-        this.cost = otherSkills;
-        this.costForTurn = this.cost;
+    public void atTurnStart() {
+        super.atTurnStart();
     }
 
     @Override
-    public void update() {
-        super.update();
-        if (AbstractDungeon.player != null && AbstractDungeon.getCurrMapNode() != null && AbstractDungeon.getCurrRoom() != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
-            applyPowers();
-            this.isCostModified = true;
-        }else{
-            this.cost = 0;
-            this.costForTurn = 0;
-            this.isCostModified = false;
+    public void onPlayCard(AbstractCard c, AbstractMonster m) {
+        super.onPlayCard(c, m);
+        if(c != this) {
+            if (c.type == CardType.SKILL) {
+                //skillsThisTurn++;
+                this.costForTurn += 2;
+            }
+            if (c.type == CardType.ATTACK) {
+                //attacksThisTurn++;
+                this.costForTurn = Math.max(0, this.costForTurn - 1);
+
+            }
         }
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        super.triggerWhenDrawn();
+        applyPowers();
     }
 
     @Override
