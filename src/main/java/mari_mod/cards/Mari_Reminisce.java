2e52599f8d9b68600e9d7e2cf2c03ab7fbf8a85e
iryan72 <iryan0702@gmail.com>
2019-12-18 05:24:31 -0800

    Change card Reminisce to have Ethereal

--- src/main/java/mari_mod/cards/Mari_Reminisce.java
+++ src/main/java/mari_mod/cards/Mari_Reminisce.java
@@ -43,6 +43,7 @@ public class Mari_Reminisce extends AbstractMariCard {
         this.baseMagicNumber = CHOICES;
         this.magicNumber = this.baseMagicNumber;
         this.exhaust = false;
+        this.isEthereal = true;
     }
 
     @Override
