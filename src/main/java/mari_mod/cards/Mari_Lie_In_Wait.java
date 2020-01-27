6b1e11b80031b43745b1832860853f52dadb0645
iryan72 <iryan0702@gmail.com>
2020-01-26 18:34:15 -0800

    Rework card Lie In Wait to exhaust and recall for damage instead

--- src/main/java/mari_mod/cards/Mari_Lie_In_Wait.java
+++ src/main/java/mari_mod/cards/Mari_Lie_In_Wait.java
@@ -1,7 +1,10 @@
 package mari_mod.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -15,7 +18,7 @@ import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Mari_Lie_In_Wait extends AbstractMariCard {
+public class Mari_Lie_In_Wait extends AbstractMariCard implements OnRecallCard {
     public static final Logger logger = LogManager.getLogger(Mari_Lie_In_Wait.class.getName());
     public static final String ID = "MariMod:Mari_Lie_In_Wait";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
@@ -23,10 +26,10 @@ public class Mari_Lie_In_Wait extends AbstractMariCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 1;
-    private static final int BLOCK = 8;
+    private static final int BLOCK = 10;
     private static final int UPGRADE_BLOCK = 2;
-    private static final int DAMAGE = 8;
-    private static final int UPGRADE_DAMAGE = 2;
+    private static final int DAMAGE = 12;
+    private static final int UPGRADE_DAMAGE = 3;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
@@ -37,8 +40,15 @@ public class Mari_Lie_In_Wait extends AbstractMariCard {
         this.magicNumber = this.baseMagicNumber;
         this.baseBlock = BLOCK;
         this.block = this.baseBlock;
+        this.exhaust = true;
         this.tags.add(MariCustomTags.KINDLE);
         this.isAnyTarget = true;
+        tags.add(MariCustomTags.GLARING);
+    }
+
+    @Override
+    public void onRecall() {
+        addToBot(new DamageAllEnemiesAction(AbstractDungeon.player, DamageInfo.createDamageMatrix(this.magicNumber, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
     }
 
     @Override
@@ -50,7 +60,7 @@ public class Mari_Lie_In_Wait extends AbstractMariCard {
             target = p;
         }
 
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Lie_In_Wait_Power(p, this.magicNumber), this.magicNumber));
+        //AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Lie_In_Wait_Power(p, this.magicNumber), this.magicNumber));
         if(target.hasPower(Radiance_Power.POWER_ID) && target.getPower(Radiance_Power.POWER_ID).amount >= 1){
             this.successfulKindle(target);
         }
