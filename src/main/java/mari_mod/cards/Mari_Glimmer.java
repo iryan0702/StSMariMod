5c2022d4dc306e6596f1a6be1b53ef950ba2931a
iryan72 <iryan0702@gmail.com>
2020-02-01 17:14:28 -0800

    Rework card Glimmer to Recall and be Recalled to apply Radiance

--- src/main/java/mari_mod/cards/Mari_Glimmer.java
+++ src/main/java/mari_mod/cards/Mari_Glimmer.java
@@ -9,6 +9,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import mari_mod.actions.MariRecallAction;
 import mari_mod.actions.MariReducePowerIfHavePowerAction;
 import mari_mod.patches.EphemeralCardPatch;
 import mari_mod.powers.Radiance_Power;
@@ -28,30 +29,34 @@ public class Mari_Glimmer extends AbstractMariCard implements OnRecallCard{
     private static final int UPGRADE_AMOUNT = 1;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.NONE;
 
     public Mari_Glimmer(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
         this.tags.add(MariCustomTags.RADIANCE);
         this.baseRadiance = RADIANCE;
         this.radiance = this.baseRadiance;
+        this.selfRetain = true;
         this.baseMagicNumber = AMOUNT;
         this.magicNumber = this.baseMagicNumber;
 
-        this.selfRetain = true;
         EphemeralCardPatch.EphemeralField.ephemeral.set(this, true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for(int i = 0; i < this.magicNumber; i++){
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new Radiance_Power(m, this.radiance), this.radiance));
-        }
+        addToBot(new MariRecallAction(MariRecallAction.RecallType.RADIANCE));
     }
 
     @Override
     public void onRecall() {
-        this.costForTurn = 0;
+        AbstractPlayer p = AbstractDungeon.player;
+        for(int i = 0; i < this.magicNumber; i++){
+            for(AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) addToBot(new ApplyPowerAction(mo, p, new Radiance_Power(mo, this.radiance), this.radiance));
+            addToBot(new ApplyPowerAction(p, p, new Radiance_Power(p, this.radiance), this.radiance));
+        }
+        //AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Radiance_Power(p, this.radiance), this.radiance));
+        if(this.upgraded) this.costForTurn = 1;
     }
 
     @Override
@@ -62,8 +67,9 @@ public class Mari_Glimmer extends AbstractMariCard implements OnRecallCard{
     @Override
     public void upgrade() {
         if (!this.upgraded) {
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
             upgradeName();
-            this.upgradeMagicNumber(UPGRADE_AMOUNT);
         }
     }
 }
