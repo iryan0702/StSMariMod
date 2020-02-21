0a72418ec55437ade6c8fe135f1bbec7fbe2e96b
iryan72 <iryan0702@gmail.com>
2020-02-20 19:45:09 -0800

    Rework card Emotional Episode

---
+++ src/main/java/mari_mod/actions/MariEmotionalEpisodeAction.java
@@ -0,0 +1,28 @@
+package mari_mod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MariEmotionalEpisodeAction extends AbstractGameAction {
+    public static final Logger logger = LogManager.getLogger(MariEmotionalEpisodeAction.class.getName());
+    private AbstractPower powerToApply;
+
+    public MariEmotionalEpisodeAction(int goldPerDebuff) {
+        this.actionType = ActionType.POWER;
+        this.amount = goldPerDebuff;
+    }
+
+    public void update() {
+        for(AbstractPower p: AbstractDungeon.player.powers){
+            if(p.type == AbstractPower.PowerType.DEBUFF){
+                addToTop(new MariGainGoldAction(this.amount));
+            }
+        }
+        this.isDone = true;
+    }
+}
--- src/main/java/mari_mod/cards/Mari_Emotional_Episode.java
+++ src/main/java/mari_mod/cards/Mari_Emotional_Episode.java
@@ -2,12 +2,15 @@ package mari_mod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FrailPower;
 import mari_mod.MariMod;
+import mari_mod.actions.MariEmotionalEpisodeAction;
 import mari_mod.patches.EphemeralCardPatch;
 import mari_mod.powers.Gold_Gain_Debuff_Power;
 import mari_mod.powers.Gold_Gain_Not_Damaged_Power;
@@ -21,9 +24,10 @@ public class Mari_Emotional_Episode extends AbstractMariCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 0;
-    private static final int BASE_GOLD_COST = 10;
-    private static final int GOLD_GAIN_UPGRADE = 5;
-    private static final int BASE_GOLD_GAIN = 20;
+    private static final int BLOCK = 6;
+    private static final int BLOCK_UPGRADE = 2;
+    private static final int BASE_GOLD_GAIN = 10;
+    private static final int GOLD_GAIN_UPGRADE = 2;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.NONE;
@@ -34,17 +38,19 @@ public class Mari_Emotional_Episode extends AbstractMariCard {
 
         this.baseMagicNumber = BASE_GOLD_GAIN;
         this.magicNumber = this.baseMagicNumber;
-        this.baseGoldCost = BASE_GOLD_COST;
-        this.goldCost = this.baseGoldCost;
-        EphemeralCardPatch.EphemeralField.ephemeral.set(this, true);
 
-        this.tags.add(CardTags.HEALING);
+        this.baseBlock = BLOCK;
+        this.block = this.baseBlock;
+
+        EphemeralCardPatch.EphemeralField.ephemeral.set(this, true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, 1));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Gold_Gain_Debuff_Power(p, this.magicNumber), this.magicNumber));
+        addToBot(new GainBlockAction(p, this.block));
+        addToBot(new ApplyPowerAction(p, p, new FrailPower(p, 1, false), 1));
+        addToBot(new MariEmotionalEpisodeAction(this.magicNumber));
+
         MariMod.spendGold(this.goldCost);
     }
 
@@ -52,6 +58,7 @@ public class Mari_Emotional_Episode extends AbstractMariCard {
         if (!this.upgraded) {
             upgradeName();
             upgradeMagicNumber(GOLD_GAIN_UPGRADE);
+            upgradeBlock(BLOCK_UPGRADE);
         }
     }
 }
