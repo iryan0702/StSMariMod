ff3b2988be5f119755338462085afef2dca036d0
iryan72 <iryan0702@gmail.com>
2020-03-20 06:23:45 -0700

    Create framework for new power card Formation Book

--- src/main/java/mari_mod/cards/Mari_Formation_Book.java
+++ src/main/java/mari_mod/cards/Mari_Formation_Book.java
@@ -19,21 +19,21 @@ public class Mari_Formation_Book extends AbstractMariCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
     private static final int UPGRADE_COST = 0;
-    private static final CardType TYPE = CardType.SKILL;
+    private static final CardType TYPE = CardType.POWER;
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.NONE;
 
     public Mari_Formation_Book(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-        this.exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new Mari_$Mijuku_Dreamer(),1,true,true,false));
+        /*AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new Mari_$Mijuku_Dreamer(),1,true,true,false));
         AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new Mari_$Mirai_Ticket(),1,true,true,false));
         AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new Mari_$My_Mai_Tonight(),1,true,true,false));
         AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new Mari_$Miracle_Wave(),1,true,false,false));
+        */
     }
 
     public void upgrade() {
---
+++ src/main/java/mari_mod/powers/Well_Formed_Power.java
@@ -0,0 +1,55 @@
+package mari_mod.powers;
+
+
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import mari_mod.MariMod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Well_Formed_Power extends AbstractPower
+{
+    public static final String POWER_ID = "MariMod:Well_Formed_Power";
+    private static final PowerStrings cardStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static PowerType POWER_TYPE = PowerType.BUFF;
+    public static final String NAME = cardStrings.NAME;
+    public static final String[] DESCRIPTION = cardStrings.DESCRIPTIONS;
+    public static final Logger logger = LogManager.getLogger(MariMod.class.getName());
+    public Well_Formed_Power(AbstractCreature owner, int amount)
+    {
+        this.name = NAME;
+        this.type = POWER_TYPE;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.isTurnBased = false;
+        this.canGoNegative = true;
+        this.updateDescription();
+        MariMod.setPowerImages(this);
+    }
+
+    @Override
+    public void atStartOfTurnPostDraw() {
+        super.atStartOfTurnPostDraw();
+    }
+
+    public void stackPower(int stackAmount)
+    {
+        logger.info("this stacks: " + stackAmount);
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+        if(this.amount > 100){
+            this.amount = 100;
+        }
+    }
+
+
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTION[0] + this.amount + DESCRIPTION[1];
+    }
+}
