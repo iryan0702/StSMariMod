e2e927a64347c71aee460a06c95eb24938157034
iryan72 <iryan0702@gmail.com>
2020-03-20 06:18:35 -0700

    Nerf power Withdrawl to give 4(6) Block from 5(7)

--- src/main/java/mari_mod/cards/Mari_Withdrawal.java
+++ src/main/java/mari_mod/cards/Mari_Withdrawal.java
@@ -18,7 +18,7 @@ public class Mari_Withdrawal extends AbstractMariCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int AMOUNT = 5;
+    private static final int AMOUNT = 4;
     private static final int UPGRADE_AMOUNT = 2;
     private static final CardType TYPE = CardType.POWER;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
