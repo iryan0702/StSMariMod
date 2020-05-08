4101f2012a6d4b14aa0978658c9dd0ea6e25dac7
iryan72 <iryan0702@gmail.com>
2020-05-08 06:03:36 -0700

    Rework card "Approved!" to not exhaust and also apply 1 Radiance.

--- src/main/java/mari_mod/actions/MariApprovedAction.java
+++ src/main/java/mari_mod/actions/MariApprovedAction.java
@@ -1,26 +1,30 @@
 package mari_mod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.ThoughtBubble;
+import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MariApprovedAction extends AbstractGameAction {
     public static final Logger logger = LogManager.getLogger(MariApprovedAction.class.getName());
     private AbstractMonster targetMonster;
+    public int radiance;
     private String failText;
 
-    public MariApprovedAction(int damage, AbstractMonster m, String failText) {
+    public MariApprovedAction(int damage, int radiance, AbstractMonster m, String failText) {
         this.duration = 0.0F;
         this.actionType = ActionType.WAIT;
         this.amount = damage;
         this.failText = failText;
         this.targetMonster = m;
+        this.radiance = radiance;
     }
 
     public void update() {
@@ -28,6 +32,7 @@ public class MariApprovedAction extends AbstractGameAction {
         if (this.targetMonster != null && (targetMonster.getIntentBaseDmg() >= 0)) {
             AbstractDungeon.effectList.add(new ThoughtBubble(p.dialogX, p.dialogY, 3.0F, failText, true));
         } else {
+            AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(this.targetMonster, p, new Radiance_Power(this.targetMonster, this.radiance), this.radiance));
             AbstractDungeon.actionManager.addToTop(new DamageAction(this.targetMonster, new DamageInfo(p, this.amount, DamageInfo.DamageType.NORMAL), AttackEffect.BLUNT_LIGHT));
         }
 
--- src/main/java/mari_mod/cards/Mari_Approved.java
+++ src/main/java/mari_mod/cards/Mari_Approved.java
@@ -1,5 +1,6 @@
 package mari_mod.cards;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -7,6 +8,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import mari_mod.actions.MariApprovedAction;
+import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,22 +20,23 @@ public class Mari_Approved extends AbstractMariCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 2;
-    private static final int ATTACK_DMG = 28;
-    private static final int UPGRADE_ATTACK_DMG = 7;
+    private static final int RADIANCE = 1;
+    private static final int ATTACK_DMG = 22;
+    private static final int UPGRADE_ATTACK_DMG = 5;
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
 
     public Mari_Approved(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
+        this.baseRadiance = this.radiance = RADIANCE;
         this.baseDamage = ATTACK_DMG;
         this.tags.add(MariCustomTags.QUOTATIONS);
-        this.exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new MariApprovedAction(this.damage, m, EXTENDED_DESCRIPTION[0]));
+        AbstractDungeon.actionManager.addToBottom(new MariApprovedAction(this.damage, this.radiance, m, EXTENDED_DESCRIPTION[0]));
     }
 
     @Override
