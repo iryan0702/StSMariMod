fbde5b19712bc9d8d172a9b1ad069ba570d4a36d
iryan72 <iryan0702@gmail.com>
2020-05-23 06:01:20 -0700

    Rework card Glimmer to gain Energy on Recall

--- src/main/java/mari_mod/cards/Mari_Glimmer.java
+++ src/main/java/mari_mod/cards/Mari_Glimmer.java
@@ -1,6 +1,7 @@
 package mari_mod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -13,15 +14,15 @@ import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Mari_Glimmer extends AbstractMariCard{
+public class Mari_Glimmer extends AbstractMariCard implements OnRecallCard{
     public static final Logger logger = LogManager.getLogger(Mari_Glimmer.class.getName());
     public static final String ID = "MariMod:Mari_Glimmer";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 2;
     private static final int RADIANCE = 2;
+    private static final int UPGRADE_RADIANCE = 1;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ALL;
@@ -39,13 +40,17 @@ public class Mari_Glimmer extends AbstractMariCard{
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         //addToBot(new MariRecallAction(this, new MariGlimmerFollowUpAction(this.radiance)));
-        addToBot(new MariRecallAction(this));
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Radiance_Power(p, this.radiance), this.radiance));
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Radiance_Power(p, this.radiance), this.radiance));
         for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
             AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new Radiance_Power(mo, this.radiance), this.radiance));
             AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new Radiance_Power(mo, this.radiance), this.radiance));
         }
+    }
+
+    @Override
+    public void onRecall() {
+        addToBot(new GainEnergyAction(1));
     }
 
     @Override
@@ -56,9 +61,7 @@ public class Mari_Glimmer extends AbstractMariCard{
     @Override
     public void upgrade() {
         if (!this.upgraded) {
-            this.selfRetain = true;
-            this.rawDescription = UPGRADE_DESCRIPTION;
-            this.initializeDescription();
+            upgradeRadiance(UPGRADE_RADIANCE);
             upgradeName();
         }
     }
