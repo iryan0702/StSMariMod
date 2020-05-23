6ad00d0ede53ec43bb1cf3ed21cdfd5ee35c83fa
iryan72 <iryan0702@gmail.com>
2020-05-23 06:11:30 -0700

    Delete deprecated Sparkle Follow Up Action

--- src/main/java/mari_mod/cards/Mari_Sparkle.java
+++ src/main/java/mari_mod/cards/Mari_Sparkle.java
@@ -1,19 +1,12 @@
 package mari_mod.cards;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import mari_mod.MariMod;
-import mari_mod.actions.MariSparkleFollowUpAction;
 import mari_mod.patches.EphemeralCardPatch;
 import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
