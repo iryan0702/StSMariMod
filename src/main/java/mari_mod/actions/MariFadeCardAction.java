ab2dd98492878db81d0cca9141f830a8a719dc18
iryan72 <iryan0702@gmail.com>
2020-05-23 06:00:10 -0700

    Adjust FadedCardAction to catch all in combat instances

--- src/main/java/mari_mod/actions/MariFadeCardAction.java
+++ src/main/java/mari_mod/actions/MariFadeCardAction.java
@@ -7,6 +7,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.GetAllInBattleInstances;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.ThoughtBubble;
 import mari_mod.cards.AbstractMariCard;
@@ -24,8 +25,12 @@ public class MariFadeCardAction extends AbstractGameAction {
     }
 
     public void update() {
-        fadeCard.faded = true;
-        fadeCard.setFadedStats();
+        for(AbstractCard c: GetAllInBattleInstances.get(fadeCard.uuid)) {
+            if(c instanceof AbstractMariCard){
+                ((AbstractMariCard) c).faded = true;
+                ((AbstractMariCard) c).setFadedStats();
+            }
+        }
         this.isDone = true;
     }
 }
