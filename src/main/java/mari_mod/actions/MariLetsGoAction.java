de8d34b9762515a8cb07d73486d0481d496117fd
iryan72 <iryan0702@gmail.com>
2020-06-16 20:13:01 -0700

    Rework card Lets Go, remove old card tracker functions

--- src/main/java/mari_mod/actions/MariLetsGoAction.java
+++ src/main/java/mari_mod/actions/MariLetsGoAction.java
@@ -1,10 +1,13 @@
 package mari_mod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import mari_mod.patches.MariDrawnCardsTrackerPatch;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,26 +16,47 @@ import java.util.ArrayList;
 public class MariLetsGoAction extends AbstractGameAction {
     public static final Logger logger = LogManager.getLogger(MariLetsGoAction.class.getName());
     boolean upgraded;
+    int energyOnUse;
+    boolean freeToPlayOnce;
+    AbstractPlayer p;
 
-    public MariLetsGoAction(int energyGain) {
-        this.amount = energyGain;
-        this.actionType = ActionType.CARD_MANIPULATION;
+    public MariLetsGoAction(boolean upgraded, boolean freeToPlayOnce, int energyOnUse) {
+        this.upgraded = upgraded;
+        this.duration = Settings.ACTION_DUR_XFAST;
+        this.actionType = ActionType.SPECIAL;
+        this.energyOnUse = energyOnUse;
+        this.freeToPlayOnce = freeToPlayOnce;
+        this.p = AbstractDungeon.player;
     }
 
     public void update() {
-        ArrayList<AbstractCard> cardsToFlash = new ArrayList<>();
+        int draw = EnergyPanel.totalCount;
+        int energy;
 
-        int energyGain = 0;
-        for(AbstractCard c: MariDrawnCardsTrackerPatch.drawnCards){
-            if(c.cost == 3) {
-                energyGain += this.amount;
-                cardsToFlash.add(c);
+        if (this.energyOnUse != -1) {
+            draw = this.energyOnUse;
+        }
+
+        if (this.p.hasRelic("Chemical X")) {
+            draw += 2;
+            this.p.getRelic("Chemical X").flash();
+        }
+
+        energy = Math.max(0, draw - 1);
+        if (this.upgraded) {
+            ++draw;
+        }
+
+        if (draw > 0) {
+            addToBot(new DrawCardAction(draw));
+            addToBot(new GainEnergyAction(energy));
+
+            if (!this.freeToPlayOnce) {
+                this.p.energy.use(EnergyPanel.totalCount);
             }
         }
-        if(energyGain > 0){
-            AbstractDungeon.actionManager.addToTop(new GainEnergyAction(energyGain));
-            AbstractDungeon.actionManager.addToTop(new CardFlashAction(cardsToFlash));
-        }
+
         this.isDone = true;
     }
+
 }
--- src/main/java/mari_mod/cards/Mari_Lets_Go.java
+++ src/main/java/mari_mod/cards/Mari_Lets_Go.java
@@ -7,7 +7,6 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import mari_mod.actions.MariDrawnCardsTrackerAction;
 import mari_mod.actions.MariLetsGoAction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,10 +17,8 @@ public class Mari_Lets_Go extends AbstractMariCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 1;
-    private static final int DRAW = 3;
-    private static final int UPGRADE_DRAW = 1;
-    private static final int ENERGY_GAIN = 1;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    private static final int COST = -1;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.NONE;
@@ -29,32 +26,11 @@ public class Mari_Lets_Go extends AbstractMariCard {
     public Mari_Lets_Go(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
         this.tags.add(MariCustomTags.QUOTATIONS);
-        this.baseMagicNumber = DRAW;
-        this.magicNumber = this.baseMagicNumber;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new MariDrawnCardsTrackerAction(true,false));
-        AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p,this.magicNumber));
-        AbstractDungeon.actionManager.addToBottom(new MariLetsGoAction(ENERGY_GAIN));
-        AbstractDungeon.actionManager.addToBottom(new MariDrawnCardsTrackerAction(false,true));
-    }
-
-    @Override
-    public void atTurnStart() {
-        super.atTurnStart();
-    }
-
-    @Override
-    public void applyPowers() {
-        super.applyPowers();
-    }
-
-    @Override
-    public void triggerWhenDrawn() {
-        super.triggerWhenDrawn();
-        applyPowers();
+        AbstractDungeon.actionManager.addToBottom(new MariLetsGoAction(this.upgraded, this.freeToPlayOnce, this.energyOnUse));
     }
 
     @Override
@@ -66,7 +42,8 @@ public class Mari_Lets_Go extends AbstractMariCard {
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_DRAW);
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 }
