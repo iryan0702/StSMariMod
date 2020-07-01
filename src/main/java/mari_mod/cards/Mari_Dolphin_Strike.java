fc96982b571aa5f5989de9ee935ca5dc07250050
iryan72 <iryan0702@gmail.com>
2020-06-30 19:28:40 -0700

    6/25: Rework card Dolphin Strike

--- src/main/java/mari_mod/cards/Mari_Dolphin_Strike.java
+++ src/main/java/mari_mod/cards/Mari_Dolphin_Strike.java
@@ -2,7 +2,10 @@ package mari_mod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ModifyDamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -17,53 +20,42 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 
-public class Mari_Dolphin_Strike extends AbstractMariCard {
+public class Mari_Dolphin_Strike extends AbstractMariCard implements OnRecallCard{
     public static final Logger logger = LogManager.getLogger(Mari_Dolphin_Strike.class.getName());
     public static final String ID = "MariMod:Mari_Dolphin_Strike";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int ATTACK_DMG = 5;
-    private static final int DAMAGE_TIMES = 2;
-    private static final int DAMAGE_TIMES_UPGRADE = 1;
+    private static final int ATTACK_DMG = 4;
+    private static final int ATTACK_DMG_UPGRADE = 1;
+    private static final int ATTACK_DMG_GROWTH = 3;
+    private static final int ATTACK_DMG_GROWTH_UPGRADE = 1;
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
 
     public Mari_Dolphin_Strike(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
         this.baseDamage = ATTACK_DMG;
         this.damage = this.baseDamage;
-        this.baseMagicNumber = DAMAGE_TIMES;
+        this.baseMagicNumber = ATTACK_DMG_GROWTH;
         this.magicNumber = this.baseMagicNumber;
-        this.isAnyTarget = true;
+
+        this.isEthereal = true;
         this.tags.add(CardTags.STRIKE);
-        this.tags.add(MariCustomTags.KINDLE);
+        this.tags.add(MariCustomTags.GLARING);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractCreature target;
-        if(m != null) {
-            target = m;
-        }else{
-            target = p;
-        }
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
 
-        if(target.hasPower(Radiance_Power.POWER_ID) && target.getPower(Radiance_Power.POWER_ID).amount >= 1){
-            this.successfulKindle(target);
-        }
-        ArrayList<AbstractGameAction> strikes = new ArrayList<>();
-        ArrayList<AbstractGameAction> strikesPlusOne = new ArrayList<>();
-        strikesPlusOne.add(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
-        for(int i = 0; i < this.magicNumber; i++){
-            strikes.add(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-            strikesPlusOne.add(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        }
-        AbstractDungeon.actionManager.addToBottom(new MariUnsuccessfulKindleAction(target, strikes));
-        AbstractDungeon.actionManager.addToBottom(new MariSuccessfulKindleAction(target, strikesPlusOne));
-
+    @Override
+    public void onRecall() {
+        addToBot(new ModifyDamageAction(this.uuid, this.magicNumber));
     }
 
     @Override
@@ -75,7 +67,8 @@ public class Mari_Dolphin_Strike extends AbstractMariCard {
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeMagicNumber(DAMAGE_TIMES_UPGRADE);
+            upgradeDamage(ATTACK_DMG_UPGRADE);
+            upgradeMagicNumber(ATTACK_DMG_GROWTH_UPGRADE);
         }
     }
 }
