6d0b86eb53c3da8010cb3510fdc058f7c0226f3c
iryan72 <iryan0702@gmail.com>
2020-07-09 05:28:20 -0700

    Change cards Char Dev and Choreography to gain [E] on upgrade

--- src/main/java/mari_mod/cards/Mari_Character_Development.java
+++ src/main/java/mari_mod/cards/Mari_Character_Development.java
@@ -1,6 +1,7 @@
 package mari_mod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -16,8 +17,8 @@ public class Mari_Character_Development extends AbstractMariCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 2;
-    private static final int UPGRADE_COST = 1;
     private static final CardType TYPE = CardType.POWER;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.NONE;
@@ -28,13 +29,17 @@ public class Mari_Character_Development extends AbstractMariCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if(upgraded){
+            addToBot(new GainEnergyAction(1));
+        }
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Character_Development_Power(p, 2), 2));
     }
 
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeBaseCost(UPGRADE_COST);
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 
