ad159d57def97f4cbf82aafb8196d9b8c1a3d1fd
iryan72 <iryan0702@gmail.com>
2020-07-16 19:33:45 -0700

    Remove Exhaust and CardMod from card Slap!

--- src/main/java/mari_mod/cards/Mari_Slap.java
+++ src/main/java/mari_mod/cards/Mari_Slap.java
@@ -1,11 +1,8 @@
 package mari_mod.cards;
 
-import basemod.abstracts.AbstractCardModifier;
-import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -24,27 +21,25 @@ public class Mari_Slap extends AbstractMariCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
     private static final int DAMAGE = 9;
-    private static final int UPGRADE_DAMAGE = 2;
+    private static final int UPGRADE_DAMAGE = 0;
     private static final int SCALING = 1;
     private static final int UPGRADE_SCALING = 1;
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
-    private SlapMod slapMod;
+    private int veryBaseDamage;
 
     public Mari_Slap(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
 
         this.baseDamage = DAMAGE;
-        this.damage = this.baseDamage;
+        veryBaseDamage = this.damage = this.baseDamage;
 
         this.baseMagicNumber = SCALING;
         this.magicNumber = this.baseMagicNumber;
 
-        slapMod = new SlapMod();
-        CardModifierManager.addModifier(this, slapMod);
-
-        this.exhaust = true;
+//        slapMod = new SlapMod();
+//        CardModifierManager.addModifier(this, slapMod);
     }
 
     @Override
@@ -53,10 +48,11 @@ public class Mari_Slap extends AbstractMariCard {
         if(this.damage > this.baseDamage * 2) effect = AbstractGameAction.AttackEffect.BLUNT_HEAVY;
         if(this.damage > this.baseDamage * 5) AbstractDungeon.actionManager.addToBottom(new VFXAction(new VerticalImpactEffect(m.hb.cX + m.hb.width / 4.0F, m.hb.cY - m.hb.height / 4.0F)));;
         AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), effect));
+        this.baseDamage = veryBaseDamage;
     }
 
     public void boost(){
-        slapMod.boost += this.magicNumber;
+        this.baseDamage += this.magicNumber;
     }
 
     public void upgrade() {
@@ -64,27 +60,34 @@ public class Mari_Slap extends AbstractMariCard {
             upgradeName();
             upgradeMagicNumber(UPGRADE_SCALING);
             upgradeDamage(UPGRADE_DAMAGE);
+            veryBaseDamage += UPGRADE_DAMAGE;
             this.initializeDescription();
         }
     }
 
-    public class SlapMod extends AbstractCardModifier{
-
-        public int boost;
-
-        @Override
-        public boolean isInherent(AbstractCard card) {
-            return true;
-        }
-
-        @Override
-        public float modifyDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
-            return boost + super.modifyDamage(damage, type, card, target);
-        }
-
-        @Override
-        public AbstractCardModifier makeCopy() {
-            return new SlapMod();
-        }
-    }
+//    public class SlapMod extends AbstractCardModifier{
+//
+//        public int boost;
+//
+//        @Override
+//        public boolean isInherent(AbstractCard card) {
+//            return true;
+//        }
+//
+//        @Override
+//        public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+//            super.onUse(card, target, action);
+//            this.boost = 0;
+//        }
+//
+//        @Override
+//        public float modifyDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+//            return boost + super.modifyDamage(damage, type, card, target);
+//        }
+//
+//        @Override
+//        public AbstractCardModifier makeCopy() {
+//            return new SlapMod();
+//        }
+//    }
 }
