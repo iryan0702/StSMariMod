f14dff62e93055077d82c9fd566382664dbd4d0a
iryan72 <iryan0702@gmail.com>
2020-07-16 19:30:56 -0700

    Rework card Overexertion to draw until a # of playable cards in hand

--- src/main/java/mari_mod/actions/MariOverexertionDrawFollowUpAction.java
+++ src/main/java/mari_mod/actions/MariOverexertionDrawFollowUpAction.java
@@ -1,9 +1,10 @@
 package mari_mod.actions;
 
+import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,20 +12,23 @@ import org.apache.logging.log4j.Logger;
 public class MariOverexertionDrawFollowUpAction extends AbstractGameAction {
     public static final Logger logger = LogManager.getLogger(MariOverexertionDrawFollowUpAction.class.getName());
 
-    public MariOverexertionDrawFollowUpAction() {
+    public MariOverexertionDrawFollowUpAction(int upTo) {
         this.duration = 0.001F;
+        this.amount = upTo;
     }
 
     public void update() {
-        AbstractDungeon.actionManager.addToTop(new WaitAction(0.4F));
         this.tickDuration();
-        if (this.isDone) {
-
-            for(AbstractCard c: DrawCardAction.drawnCards){
-                if(c.costForTurn >= 1){
-                    c.costForTurn = 1;
-                }
+        AbstractPlayer p = AbstractDungeon.player;
+        int usable = 0;
+        for(AbstractCard c: p.hand.group){
+            if(c.canUse(p, null)){
+                usable++;
             }
+        }
+
+        if(usable < this.amount && p.hand.size() < BaseMod.MAX_HAND_SIZE){
+            addToTop(new DrawCardAction(1, new MariOverexertionDrawFollowUpAction(this.amount)));
         }
 
     }
--- src/main/java/mari_mod/cards/Mari_Overexertion.java
+++ src/main/java/mari_mod/cards/Mari_Overexertion.java
@@ -1,15 +1,13 @@
 package mari_mod.cards;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.FrailPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
+import mari_mod.actions.MariMakeTempCardInExhaustPileAction;
 import mari_mod.actions.MariOverexertionDrawFollowUpAction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,17 +18,19 @@ public class Mari_Overexertion extends AbstractMariCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 1;
-    private static final int DRAW = 3;
-    private static final int UPGRADE_DRAW = 1;
+    private static final int UP_TO = 4;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
 
     public Mari_Overexertion(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-        this.baseMagicNumber = DRAW;
+        this.baseMagicNumber = UP_TO;
         this.magicNumber =  this.baseMagicNumber;
+
+        this.cardsToPreview = new Mari_Repression();
     }
 
     @Override
@@ -40,9 +40,14 @@ public class Mari_Overexertion extends AbstractMariCard {
         AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p,this.magicNumber));
         AbstractDungeon.actionManager.addToBottom(new MariSetCostReductionOnDrawAction(0,1));
         */
-        AbstractDungeon.actionManager.addToBottom(new DrawCardAction(this.magicNumber, new MariOverexertionDrawFollowUpAction()));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p,p,new VulnerablePower(p,1,false),1));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p,p,new FrailPower(p,1,false),1));
+
+        addToBot(new MariMakeTempCardInExhaustPileAction(new Mari_Repression(), 1, false, true));
+
+        if(!upgraded){
+            AbstractDungeon.actionManager.addToBottom(new GainEnergyAction(1));
+        }
+
+        AbstractDungeon.actionManager.addToBottom(new MariOverexertionDrawFollowUpAction(this.magicNumber));
     }
 
     @Override
@@ -54,7 +59,8 @@ public class Mari_Overexertion extends AbstractMariCard {
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_DRAW);
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 }
