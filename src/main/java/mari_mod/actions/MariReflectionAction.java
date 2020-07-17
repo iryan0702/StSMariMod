1b5baf1adc31b62a1b9e40228357e53f31b58110
iryan72 <iryan0702@gmail.com>
2020-07-16 19:35:24 -0700

    Rework c. Reflection to apply player and monster Radiance to each other

--- src/main/java/mari_mod/actions/MariReflectionAction.java
+++ src/main/java/mari_mod/actions/MariReflectionAction.java
@@ -5,55 +5,68 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.ArrayList;
 
 public class MariReflectionAction extends AbstractGameAction {
     public static final Logger logger = LogManager.getLogger(MariReflectionAction.class.getName());
     private static final float DURATION = 0.01F;
     public boolean upgraded;
+    public boolean faded;
 
-    public MariReflectionAction(boolean upgraded, AbstractCreature target) {
+    public MariReflectionAction(AbstractCreature target, boolean upgraded, int fadedRadiance, boolean faded) {
         this.upgraded = upgraded;
         this.target = target;
+        this.faded = faded;
+        this.amount = fadedRadiance;
     }
 
     public void update() {
 
         AbstractPlayer p = AbstractDungeon.player;
 
-        if(this.target.hasPower(Radiance_Power.POWER_ID)) {
-
-            ArrayList<AbstractCreature> allOthers = new ArrayList<>();
-
-            if(!this.target.equals(p)){
-                allOthers.add(p);
-            }
-
-            for(AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters) {
-                if(!monster.isDead && !monster.halfDead && !this.target.equals(monster)) {
-                    allOthers.add(monster);
-                }
-            }
-
-            int currRadiance = this.target.getPower(Radiance_Power.POWER_ID).amount;
-
-            if(currRadiance > 0 && allOthers.size() > 0) {
-                if (this.upgraded) {
-                    for(AbstractCreature other: allOthers){
-                        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(other, p, new Radiance_Power(other, currRadiance), currRadiance));
-                    }
-                } else {
-                    AbstractCreature luckySoul = allOthers.get(AbstractDungeon.cardRandomRng.random(0,allOthers.size()-1));
-                    AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(luckySoul, p, new Radiance_Power(luckySoul, currRadiance), currRadiance));
-                }
-
+        if(this.upgraded) {
+            AbstractPower mp = this.target.getPower(Radiance_Power.POWER_ID);
+            if (mp != null) {
+                if(mp.amount >= 1) addToTop(new ApplyPowerAction(p, p, new Radiance_Power(p, this.faded ? this.amount : mp.amount), this.faded ? this.amount : mp.amount));
             }
         }
+
+        AbstractPower pp = p.getPower(Radiance_Power.POWER_ID);
+        if(pp != null){
+            if(pp.amount >= 1) addToTop(new ApplyPowerAction(this.target, p, new Radiance_Power(this.target, this.faded ? this.amount : pp.amount), this.faded ? this.amount : pp.amount));
+        }
+
+//        if(this.target.hasPower(Radiance_Power.POWER_ID)) {
+//
+//            ArrayList<AbstractCreature> allOthers = new ArrayList<>();
+//
+//            if(!this.target.equals(p)){
+//                allOthers.add(p);
+//            }
+//
+//            for(AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters) {
+//                if(!monster.isDead && !monster.halfDead && !this.target.equals(monster)) {
+//                    allOthers.add(monster);
+//                }
+//            }
+//
+//            int currRadiance = this.target.getPower(Radiance_Power.POWER_ID).amount;
+//
+//            if(currRadiance > 0 && allOthers.size() > 0) {
+//                if (this.upgraded) {
+//                    for(AbstractCreature other: allOthers){
+//                        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(other, p, new Radiance_Power(other, currRadiance), currRadiance));
+//                    }
+//                } else {
+//                    AbstractCreature luckySoul = allOthers.get(AbstractDungeon.cardRandomRng.random(0,allOthers.size()-1));
+//                    AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(luckySoul, p, new Radiance_Power(luckySoul, currRadiance), currRadiance));
+//                }
+//
+//            }
+//        }
         this.isDone = true;
     }
 }
--- src/main/java/mari_mod/cards/Mari_Reflection.java
+++ src/main/java/mari_mod/cards/Mari_Reflection.java
@@ -2,13 +2,13 @@ package mari_mod.cards;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import mari_mod.actions.MariReflectionAction;
 import mari_mod.actions.MariSpendGoldAction;
+import mari_mod.patches.EphemeralCardPatch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,11 +19,12 @@ public class Mari_Reflection extends AbstractMariCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 1;
     private static final int BASE_GOLD_COST = 5;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
 
     public Mari_Reflection(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
@@ -32,20 +33,21 @@ public class Mari_Reflection extends AbstractMariCard {
 
         this.baseGoldCost = BASE_GOLD_COST;
         this.goldCost = this.baseGoldCost;
-        this.isAnyTarget = true;
-        this.exhaust = true;
+//        this.isAnyTarget = true;
+//        this.exhaust = true;
+        EphemeralCardPatch.EphemeralField.ephemeral.set(this, true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new MariSpendGoldAction(this));
-        AbstractCreature target;
-        if(m != null) {
-            target = m;
-        }else{
-            target = p;
-        }
-        AbstractDungeon.actionManager.addToBottom(new MariReflectionAction(this.upgraded,target));
+        AbstractDungeon.actionManager.addToBottom(new MariReflectionAction(m, this.upgraded, this.radiance, this.faded));
+    }
+
+    @Override
+    public void setFadedStats() {
+        this.rawDescription = this.upgraded ? EXTENDED_DESCRIPTION[1] : EXTENDED_DESCRIPTION[0];
+        super.setFadedStats();
     }
 
     @Override
@@ -56,8 +58,6 @@ public class Mari_Reflection extends AbstractMariCard {
     @Override
     public void upgrade() {
         if (!this.upgraded) {
-            this.target = CardTarget.ALL_ENEMY;
-            this.exhaust = false;
             upgradeName();
             this.rawDescription = UPGRADE_DESCRIPTION;
             this.initializeDescription();
