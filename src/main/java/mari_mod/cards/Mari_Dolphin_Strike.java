1ba19e8b119c9841f26a633128690ffc5173f1fb
iryan72 <iryan0702@gmail.com>
2021-02-09 08:40:30 -0800

    Rework card Dolphin Strike to Reflect for high energy

--- src/main/java/mari_mod/cards/Mari_Dolphin_Strike.java
+++ src/main/java/mari_mod/cards/Mari_Dolphin_Strike.java
@@ -1,24 +1,16 @@
 package mari_mod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.ModifyDamageAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import mari_mod.actions.MariSuccessfulKindleAction;
-import mari_mod.actions.MariUnsuccessfulKindleAction;
-import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.ArrayList;
 
 public class Mari_Dolphin_Strike extends AbstractMariCard implements OnRecallCard{
     public static final Logger logger = LogManager.getLogger(Mari_Dolphin_Strike.class.getName());
@@ -27,10 +19,9 @@ public class Mari_Dolphin_Strike extends AbstractMariCard implements OnRecallCar
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int ATTACK_DMG = 4;
-    private static final int ATTACK_DMG_UPGRADE = 1;
-    private static final int ATTACK_DMG_GROWTH = 3;
-    private static final int ATTACK_DMG_GROWTH_UPGRADE = 1;
+    private static final int ATTACK_DMG = 6;
+    private static final int ATTACK_DMG_UPGRADE = 3;
+    private static final int ENERGY_GAIN = 2;
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
@@ -39,10 +30,8 @@ public class Mari_Dolphin_Strike extends AbstractMariCard implements OnRecallCar
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
         this.baseDamage = ATTACK_DMG;
         this.damage = this.baseDamage;
-        this.baseMagicNumber = ATTACK_DMG_GROWTH;
-        this.magicNumber = this.baseMagicNumber;
 
-        this.isEthereal = true;
+        this.exhaust = true;
         this.tags.add(CardTags.STRIKE);
         this.tags.add(MariCustomTags.GLARING);
     }
@@ -50,12 +39,11 @@ public class Mari_Dolphin_Strike extends AbstractMariCard implements OnRecallCar
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
-        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 
     @Override
     public void onRecall() {
-        addToBot(new ModifyDamageAction(this.uuid, this.magicNumber));
+        addToBot(new GainEnergyAction(ENERGY_GAIN));
     }
 
     @Override
@@ -68,7 +56,6 @@ public class Mari_Dolphin_Strike extends AbstractMariCard implements OnRecallCar
         if (!this.upgraded) {
             upgradeName();
             upgradeDamage(ATTACK_DMG_UPGRADE);
-            upgradeMagicNumber(ATTACK_DMG_GROWTH_UPGRADE);
         }
     }
 }
