b03cfab59b8a6ea9f5d972aa7f6c42f80e035ac7
iryan72 <iryan0702@gmail.com>
2021-02-15 05:07:25 -0800

    Nerf card Spark Damage

--- src/main/java/mari_mod/cards/Mari_Practice_Outfit.java
+++ src/main/java/mari_mod/cards/Mari_Practice_Outfit.java
@@ -1,6 +1,7 @@
 package mari_mod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -17,7 +18,6 @@ public class Mari_Practice_Outfit extends AbstractMariCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 2;
-    private static final int UPGRADE_COST = 1;
     private static final int BLOCK = 20;
     private static final CardType TYPE = CardType.POWER;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
@@ -34,6 +34,9 @@ public class Mari_Practice_Outfit extends AbstractMariCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if(upgraded){
+            addToBot(new GainEnergyAction(1));
+        }
 
         addToBot(new ApplyPowerAction(p, p, new Practice_Outfit_Power(p)));
 //        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block, true ));
@@ -61,8 +64,9 @@ public class Mari_Practice_Outfit extends AbstractMariCard {
 
     public void upgrade() {
         if (!this.upgraded) {
-            upgradeBaseCost(UPGRADE_COST);
             upgradeName();
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 }
