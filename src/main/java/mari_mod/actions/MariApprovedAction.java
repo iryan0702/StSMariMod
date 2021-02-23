bc50d72d7d573c5fb7b06ac981b1ee7911c83911
iryan72 <iryan0702@gmail.com>
2021-02-23 10:47:56 -0800

    Change card Approved to apply extra Radiance to blocking characters

--- src/main/java/mari_mod/actions/MariApprovedAction.java
+++ src/main/java/mari_mod/actions/MariApprovedAction.java
@@ -2,34 +2,31 @@ package mari_mod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.ThoughtBubble;
 import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MariApprovedAction extends AbstractGameAction {
     public static final Logger logger = LogManager.getLogger(MariApprovedAction.class.getName());
-    private AbstractMonster targetMonster;
     public int radiance;
 
-    public MariApprovedAction(int radiance, AbstractMonster m) {
+    public MariApprovedAction(int radiance) {
         this.duration = 0.0F;
         this.actionType = ActionType.POWER;
-        this.amount = radiance;
-        this.targetMonster = m;
+        this.radiance = radiance;
     }
 
     public void update() {
         AbstractPlayer p = AbstractDungeon.player;
-
-        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(this.targetMonster, p, new Radiance_Power(this.targetMonster, this.radiance), this.radiance));
-        AbstractDungeon.actionManager.addToTop(new DamageAction(this.targetMonster, new DamageInfo(p, this.amount, DamageInfo.DamageType.NORMAL), AttackEffect.BLUNT_LIGHT));
-
+        addToTop(new ApplyPowerAction(p, p, new Radiance_Power(p, this.radiance), this.radiance));
+        if(p.currentBlock > 0) addToTop(new ApplyPowerAction(p, p, new Radiance_Power(p, this.radiance), this.radiance));
+        for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            addToTop(new ApplyPowerAction(mo, p, new Radiance_Power(mo, this.radiance), this.radiance));
+            if(mo.currentBlock > 0) addToTop(new ApplyPowerAction(mo, p, new Radiance_Power(mo, this.radiance), this.radiance));
+        }
         this.isDone = true;
     }
 }
--- src/main/java/mari_mod/cards/Mari_Approved.java
+++ src/main/java/mari_mod/cards/Mari_Approved.java
@@ -1,61 +1,55 @@
 package mari_mod.cards;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.RemoveAllBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import mari_mod.actions.MariApprovedAction;
 import mari_mod.patches.EphemeralCardPatch;
-import mari_mod.powers.Radiance_Power;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Mari_Approved extends AbstractMariCard {
+public class Mari_Approved extends AbstractMariCard{
     public static final Logger logger = LogManager.getLogger(Mari_Approved.class.getName());
     public static final String ID = "MariMod:Mari_Approved";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final int BASE_RADIANCE = 3;
     private static final int UPGRADE_BASE_RADIANCE = 1;
-    private static final int RADIANCE_SCALING = 7;
-    private static final int UPGRADE_RADIANCE_SCALING = -2;
+//    private static final int BLOCK = 7;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
-    private int realBaseRadiance;
+    private static final CardTarget TARGET = CardTarget.ALL;
 
     public Mari_Approved(){
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-        this.realBaseRadiance = this.baseRadiance = this.radiance = BASE_RADIANCE;
-        this.baseMagicNumber = this.magicNumber = RADIANCE_SCALING;
+        this.baseRadiance = this.radiance = BASE_RADIANCE;
+//        this.baseMagicNumber = this.magicNumber = BLOCK;
         this.tags.add(MariCustomTags.QUOTATIONS);
         this.tags.add(MariCustomTags.RADIANCE);
         EphemeralCardPatch.EphemeralField.ephemeral.set(this, true);
-        this.isAnyTarget = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractCreature target;
-        if(m != null) {
-            target = m;
-        }else{
-            target = p;
-        }
-        addToBot(new ApplyPowerAction(target, p, new Radiance_Power(target, this.radiance), this.radiance));
-        addToBot(new RemoveAllBlockAction(target, p));
+        addToBot(new MariApprovedAction(this.radiance));
     }
 
-    @Override
+//    @Override
+//    public void onRecall() {
+//        AbstractPlayer p = AbstractDungeon.player;
+//        addToBot(new GainBlockAction(p, p, this.magicNumber, true ));
+//        for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+//            addToBot(new GainBlockAction(mo, p, this.magicNumber, true ));
+//        }
+//    }
+
+    //useful code for block scaling effect
+    /*@Override
     public void calculateCardDamage(AbstractMonster mo) {
         this.baseRadiance = this.realBaseRadiance;
 
@@ -89,7 +83,7 @@ public class Mari_Approved extends AbstractMariCard {
         super.applyPowers();
         this.baseRadiance = this.realBaseRadiance;
         this.modifiedRadiance = this.baseRadiance != this.radiance;
-    }
+    }*/
 
     @Override
     public AbstractCard makeCopy() {
@@ -101,8 +95,6 @@ public class Mari_Approved extends AbstractMariCard {
         if (!this.upgraded) {
             upgradeName();
             upgradeRadiance(UPGRADE_BASE_RADIANCE);
-            upgradeMagicNumber(UPGRADE_RADIANCE_SCALING);
-            this.realBaseRadiance += UPGRADE_BASE_RADIANCE;
         }
     }
 }
